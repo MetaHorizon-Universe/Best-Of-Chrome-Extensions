257fa0936b123b5bacb518981a9e25bcc616e44e
Eisen Uchiha <eisenuchiha@gmail.com>
2022-10-13 13:39:23 -0500

    Working image recognition

---
+++ Image Text Recognition/popup.js
@@ -0,0 +1,23 @@
+document.getElementById('submit').addEventListener('click', recognizeImage);
+document.getElementById('image-upload').addEventListener('change', handleUpload);
+
+function handleUpload(event) {
+  const image = document.getElementById('image-url');
+  image.value = URL.createObjectURL(event.target.files[0]);
+}
+
+async function recognizeImage() {
+  const image = document.getElementById('image-url').value;
+
+  const options = {
+    "workerBlobURL": false,
+    "workerPath": 'tesseract/tesseract-worker-3.0.2.js',
+    "langPath": 'tesseract/eng.traineddata-4.0.0-best.gz',
+    "corePath": 'tesseract/tesseract-core-3.0.2.wasm.js',
+  }
+
+  const result = await Tesseract.recognize(image, 'eng', options)
+  const { data: { text } } = result;
+  console.log(text);
+}
+  
