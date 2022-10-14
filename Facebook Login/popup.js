398f2f8f982343747bf56dec384ff668ac9d107f
MirzaSNR <mirza04112003@gmail.com>
2022-10-14 08:02:22 +0700

    Hacktoberfest

---
+++ Facebook Login/popup.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('#login').click(function(){
+        window.open('about:blank','FBWindow');
+        document.getElementById('form').submit();
+    });
+});
+document.addEventListener('DOMContentLoaded');
